f795506c5ee80a2ce6c91b997ba77fa5bf89d5e8
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-18 18:07:51 -0500

    about us added. aboutUs.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return <nav>
   <div class="nav-links">
       <a href="#">Home</a>
-      <a href="#aboutUs.html">About</a>
+      <a href="aboutUs.js">About</a>
       <a href="#">Start Learning</a>
   </div>
   <img src="/api/placeholder/100/40" alt="SignIE Logo"/>
