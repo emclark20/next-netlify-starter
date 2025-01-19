e983023c9b924b470e32c9b3c5db7e9da21a2238
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-19 10:31:43 -0500

    fix global css placement. needs to be imported in app not index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer.js'
-import Styles from '@styles/globals.css'
 export default function Home() {
   return (
     <div>
