08aa69643af4bb65cec9ad72afa0bb9dea85b186
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-19 10:15:15 -0500

    reverted header to original from template

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,4 @@
-export default function Header() {
-  return 
-  <nav>
-  <div class="nav-links">
-      <a href="#">Home</a>
-      <a href="aboutUs.js">About</a>
-      <a href="#">Start Learning</a>
-  </div>
-  <img src="/api/placeholder/100/40" alt="SignIE Logo"/>
-  <a href="#" class="sign-in-btn">Sign In</a>
-</nav>
+ export default function Header({ title }) {
+    return <h1 className="title">{title}</h1>
+  }
 
-}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '@components/Header.js'
+import Header from '@components/Header'
 import Footer from '@components/Footer.js'
 
 export default function Home() {
