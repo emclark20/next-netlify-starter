a0bd9cbc443fbb1f11ce40899893b77175515b43
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-19 10:45:56 -0500

    changed path

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@
     return (<nav>
     <div class="nav-links">
         <a href="#">Home</a>
-        <a href="aboutUs.js">About</a>
+        <a href="./pages/aboutUs.js">About</a>
         <a href="#">Start Learning</a>
     </div>
     <img src="/api/placeholder/100/40" alt="SignIE Logo"/>
