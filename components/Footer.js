98289f7489b3acf632627726884d0e4ba18e4b8b
AndresJesusRodriguez <arod21199@gmail.com>
2025-01-28 18:48:39 -0500

    adjusted footer css

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer class="footer">
         <div class="footer-logo">
           <p>©2025 SignIE</p>
-          <img src="/api/placeholder/100/40" alt="SignIE Logo" />
+          <img class="footer-logo-image" src="/api/placeholder/100/40" alt="SignIE Logo" />
         </div>
         <div class="footer-links">
           <a href="#">Home</a>
