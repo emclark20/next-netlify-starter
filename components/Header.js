b583986cf3d63f0bf7f6bf6711264d7800e0f79a
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-28 13:04:57 -0500

    mass changes dont even know what it will don

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,11 @@
     return (<nav>
     <div class="nav-links">
         <a href="#">Home</a>
-        <a href="./pages/aboutUs.js">About</a>
+        <a href="aboutUs">About</a>
         <a href="#">Start Learning</a>
     </div>
     <img src="/api/placeholder/100/40" alt="SignIE Logo"/>
-    <a href="#" class="sign-in-btn">Sign In</a>
+    <a href="#" class="sign-in-btn">Sign In TEST</a>
 </nav> )
   }
 
