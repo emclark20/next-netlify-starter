cb72c8b06d9fdc98a271405375315b8e2a7452aa
Clark <127258815+emclark20@users.noreply.github.com>
2025-01-29 16:52:15 -0500

    fixed footer and header components

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <>
-      <footer class="footer">
-        <div class="footer-logo">
-          <p>©2025 SignIE</p>
-          <img class="footer-logo-image" src="/api/placeholder/100/40" alt="SignIE Logo" />
+    <footer>
+      <div className="footer-logo">
+        <Image src="/assets/primaryLogoWhite.png" width={149.28} height={42.05} alt="SignIE Logo" />
+      </div>
+      <div className="footer-container">
+        <p>©2025 SignIE</p>
+        <div className="footer-links">
+          <Link href="#">Home</Link>
+          <Link href="#">About</Link>
+          <Link href="#">Contact</Link>
         </div>
-        <div class="footer-links">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   )
 }
