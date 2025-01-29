a87bc190f596ae3fc0216f186ab8b98e43abc25a
Leigh Adams <spacecanari@gmail.com>
2025-01-29 14:06:57 -0500

    learningA pg

---
+++ pages/learningPgs50/learningA.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+
+export default function learningA() {
+  return (
+    <div>
+      <Header />
+      <div class="learningContainer">
+        <div class="video"></div>
+        <div class="catalogueA">
+          <div class="library">
+            <div class="row">
+              <button id="prev">&#10094;</button>
+              <div class="slideHolder" id="alphaB"></div>
+              <button id="next">&#10095;</button>
+            </div>
+          </div>
+        </div>
+        <div class="cv-view"></div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
