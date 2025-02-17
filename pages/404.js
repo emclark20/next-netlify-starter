f29b396254d9b48fefc3854b953bf6762f398cc1
Ka436950 <ka436950@ucf.edu>
2025-02-17 15:05:46 -0500

    made the error page

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+export default function Custom404() {
+    return <h1>404 - Page Not Found</h1>
+  }
+
+  //the code above was taken from the nextjs page that teaches you how to make a custom error page
