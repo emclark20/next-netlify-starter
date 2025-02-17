2918df812b947e4a4f870546d0c777cb664ad989
Ka436950 <ka436950@ucf.edu>
2025-02-17 16:31:02 -0500

    added a button to index and styled it

--- pages/404.js
+++ pages/404.js
@@ -14,14 +14,32 @@ export default function Custom404() {
         justifyContent: 'center',
       },
       content: {
+        position: 'absolute',
+        bottom: '10vh', // Position from bottom
+        left: '50%',
+        transform: 'translateX(-50%)', // Center horizontally
         textAlign: 'center',
+        width: '100%',
+        padding: '20px',
+      },
+      button: {
+        backgroundColor: '#ECA76B',
+        padding: ' 0.5rem 1rem',
+        fontFamily: 'var(--font-league-spartan), -apple-system, BlinkMacSystemFont, Roboto, sans-serif;',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        color: 'white',
+        ':hover': {
+            backgroundColor: 'rgba(255, 255, 255, 0.2)',
+            transform: 'scale(1.05)',
+          }
       }
     };
   
     return (
       <div style={styles.container}>
         <div style={styles.content}>
-        <Link href="/" style={styles.link}>Home</Link>
+        <Link href="/" style={styles.button}>Take me Back</Link>
         </div>
       </div>
     );
